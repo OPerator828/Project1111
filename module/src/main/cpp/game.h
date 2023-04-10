9c5a1d29f34df1661d5875dc46c39a56edee2d23
Zlsunset <122878033+Zlsunset@users.noreply.github.com>
2023-04-10 20:40:35 +0800

    Update game.h
    Signed-off-by: Zlsunset <122878033+Zlsunset@users.noreply.github.com>

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.hero.sm.m4399"
+#define GamePackageName "com.ChillyRoom.DungeonShooter"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
