d6b022a7df3397c76bb1050eda45add8fab8b279
Zlsunset <122878033+Zlsunset@users.noreply.github.com>
2025-04-07 10:49:10 +0800

    Update game.h
    Signed-off-by: Zlsunset <122878033+Zlsunset@users.noreply.github.com>

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.hd.xxdkhb.and"
+#define GamePackageName "com.jyws.xhy"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
